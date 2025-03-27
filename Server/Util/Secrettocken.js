4a004eada660d8b32e43eb4a5b7f06fbd5d44dca
arunkdorai <arunkumar.k0786@gmail.com>
2025-03-28 01:07:39 +0530

    first commit

--- Server/Util/Secrettocken.js
+++ Server/Util/Secrettocken.js
@@ -1,9 +1,8 @@
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
-dotenv.config()
+dotenv.config();
 export const createSecretToken = (id) => {
   return jwt.sign({ id }, process.env.TOKEN_KEY, {
     expiresIn: 3 * 24 * 60 * 60,
-   
   });
 };
